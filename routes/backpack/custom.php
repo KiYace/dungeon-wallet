c2589e893da0ede8f0c89912f6dfbd2de6788cc6
Kirill <themrbattlemag130599@gmail.com>
2021-05-07 09:14:22 +0300

    tags create

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,4 +14,6 @@ Route::group([
     ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    Route::crud('user', 'UserCrudController');
+    Route::crud('tags', 'TagsCrudController');
 }); // this should be the absolute last line of this file
