3c7b4578124844dee19d0745640bde261baace2e
Kirill <kiyacelerroy@gmail.com>
2021-05-18 10:04:45 +0300

    DUN-6 | delete less code

--- app/Models/Player/Level.php
+++ app/Models/Player/Level.php
@@ -2,11 +2,7 @@
 
 namespace App\Models\Player;
 
-use Backpack\CRUD\app\Models\Traits\CrudTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Laravel\Sanctum\HasApiTokens;
 
 /**
  * Class Player
--- app/Service/Player/LevelService.php
+++ app/Service/Player/LevelService.php
@@ -2,13 +2,8 @@
 
 namespace App\Service\Player;
 
-use App\DTO\AuthDTO;
-use App\Exceptions\Auth\WrongPasswordException;
-use App\Exceptions\Player\PlayerNotFoundException;
 use App\Models\Player;
 use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Hash;
 use JetBrains\PhpStorm\Pure;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
